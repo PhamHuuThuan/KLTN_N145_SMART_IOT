03e3b94b336cd57da9fa6c3b507395e10780f564
hoanghuytoi <hoanghuytoi03@gmail.com>
2025-10-04 11:44:29 +0700

    fix routes RuleRoutes

--- services/rules-service/src/routes/ruleRoutes.js
+++ services/rules-service/src/routes/ruleRoutes.js
@@ -6,11 +6,6 @@ import {
   deleteRule,
   toggleRuleStatus,
   getRuleTemplates,
-  getRulesByDevice,
-  getRulesByOwner,
-  getRuleById,
-  createBulkRules,
-  getRuleStats,
 } from '../controllers/ruleController.js';
 import { validateRuleUpdate, validateRuleStatus } from '../middleware/validation.js';
 
@@ -23,10 +18,5 @@ router.post('/', createRule);
 router.patch('/:ruleId', validateRuleUpdate, updateRule);
 router.patch('/:ruleId/status', validateRuleStatus, toggleRuleStatus);
 router.delete('/:ruleId', deleteRule);
-router.get('/stats/:ownerId', getRuleStats);
-router.get('/device/:deviceId', getRulesByDevice);
-router.get('/owner/:ownerId', getRulesByOwner);
-router.get('/:ruleId', getRuleById);
-router.post('/bulk', createBulkRules);
 
 export default router;
